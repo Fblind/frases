e0d1e03409f0dd86ae4a3ba10b19c82d40246297
Fblind <fblind@gmail.com>
2019-10-13 14:58:56 -0300

    change db config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   dbHost: process.env.NODE_ENV === "production"
-  ? `mongodb+srv://admin:${process.env.DB_PASSWORD}@cluster0-rknza.mongodb.net`
+  ? `mongodb+srv://frases-node:${process.env.DB_PASSWORD}@cluster0-rknza.mongodb.net`
   : "mongodb://localhost:27017",
   dbName: process.env.NODE_ENV === "test" ? "frases-test" : "frases",
   dbDebug: true,
